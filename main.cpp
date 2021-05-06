fb94413bdd9d0dfdce1d1b82481282c72c4183de
Hendre <hendre@mn8.ee>
2021-05-06 23:17:50 +0200

    :seedling: Hello Groot Seun

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hellos Zander" << endl;
+
+    int age;
+    cin >> age;
+
+    cout << "\n Hello Zander met ouderdom" << age << " #WINK " << endl;
+
+    return 0;
+}
